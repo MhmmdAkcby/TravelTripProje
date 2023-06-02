ecca7e6a6941eecae5b80f1a15131a19a46621e5
Lenovo <Lenovo@LENOVO>
2023-06-03 00:06:41 +0300

    Add project files.

---
+++ TravelTripProje/Migrations/202305071323046_Initial.cs
@@ -0,0 +1,18 @@
+﻿namespace TravelTripProje.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Hakkimizdas", "Aciklama", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Hakkimizdas", "Aciklama", c => c.Int(nullable: false));
+        }
+    }
+}
---
+++ TravelTripProje/Models/Siniflar/BlogYorum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProje.Models.Siniflar
+{
+    public class BlogYorum
+    {
+        public IEnumerable<Blog> Deger1 { get; set; }
+        public IEnumerable<Yorumlar> Deger2 { get; set; }
+        public IEnumerable<Blog> Deger3 { get; set; }
+    }
+}
